4976c02c7670e1951fed47114398e3e5b069d443
Amrit <asingh@elpuig.xeill.net>
2018-04-10 19:14:55 +0200

    h

--- core/src/com/mygdx/game/screens/GameScreen.java
+++ core/src/com/mygdx/game/screens/GameScreen.java
@@ -1,47 +1,50 @@
-package com.mygdx.game;
+package com.mygdx.game.screens;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Array;
+import com.mygdx.game.Controls;
 import com.mygdx.game.characters.Ball;
 import com.mygdx.game.characters.Player;
 
-public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	TextureRegion imgBackground;
-	TextureRegion imgBall;
+public class GameScreen extends MyGameScreen {
+    SpriteBatch batch;
+    TextureRegion imgBackground;
+    TextureRegion imgBall;
     TextureRegion imgPlayer;
 
-	float gravity = -.2f;
+    float gravity = -.2f;
 
-	Array<Ball> balls = new Array<Ball>();
-	Player player;
+    Array<Ball> balls = new Array<Ball>();
+    Player player;
 
-	float newBallTimer;
+    float newBallTimer;
+    public GameScreen(Game game){
+        super(game);
+    }
+    @Override
+    public void show () {
+        batch = new SpriteBatch();
+        TextureAtlas textureAtlas = new TextureAtlas("superpang.txt");
+        imgPlayer = textureAtlas.findRegion("shoot");
+        imgBall = textureAtlas.findRegion("ball0");
+        imgBackground = textureAtlas.findRegion("background");
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		TextureAtlas textureAtlas = new TextureAtlas("superpang.txt");
-		imgPlayer = textureAtlas.findRegion("shoot");
-		imgBall = textureAtlas.findRegion("ball0");
-		imgBackground = textureAtlas.findRegion("background");
+        player = new Player();
 
-		player = new Player();
-
-		balls.add(new Ball(10,10,1,12));
+        balls.add(new Ball(10,10,1,12));
         balls.add(new Ball(100,10,2,6));
         balls.add(new Ball(10,100,3,14));
-	}
+    }
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    @Override
+    public void render (float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         update();
 
@@ -52,16 +55,16 @@ public class MyGdxGame extends ApplicationAdapter {
         }
         batch.draw(imgPlayer, player.position.x, player.position.y);
         batch.end();
-	}
+    }
 
-	void update(){
+    void update(){
         updateTimers();
         addNewBall();
         updateBalls();
         updatePlayer();
     }
 
-	void updateTimers(){
+    void updateTimers(){
         newBallTimer += Gdx.graphics.getDeltaTime();
     }
 
@@ -97,9 +100,9 @@ public class MyGdxGame extends ApplicationAdapter {
             player.position.x += player.velocity.x;
         }
     }
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-	}
+
+    @Override
+    public void dispose () {
+        batch.dispose();
+    }
 }
---
+++ core/src/com/mygdx/game/screens/WelcomeScreen.java
@@ -0,0 +1,37 @@
+package com.mygdx.game.screens;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.screens.MyGameScreen;
+
+public class WelcomeScreen extends MyGameScreen {
+    SpriteBatch spriteBatch;
+    Texture logotipo;
+    float startGameTimer;
+
+    public WelcomeScreen(Game game){
+        super(game);
+    }
+
+    @Override
+    public void show() {
+        spriteBatch = new SpriteBatch();
+        logotipo = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void render(float delta) {
+        startGameTimer += delta;
+
+        if (startGameTimer > 1){
+            game.setScreen(new GameScreen(game));
+        }
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        spriteBatch.draw(logotipo, 100, 100);
+    }
+}
